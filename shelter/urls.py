0bff89907fa04418eb99550edc3f1433641ef72e
kaydence <kn0960866@otc.edu>
2026-04-30 10:43:13 -0500

    Updated urls.py to include a path for the new favorite

--- shelter/urls.py
+++ shelter/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('<int:animal_id>/share/', views.animal_share, name='animal_share'),
     path('tag/<slug:tag_slug>/', views.animal_list, name="animals_by_tag"),
     path('search/', views.animal_search, name='animal_search'),
-    path('login/', views.user_login, name='login')
+    path('login/', views.user_login, name='login'),
+    path('animals/favorites/', views.favorites_page, name='favorites'),
+    path('animals/<int:animal_id>/favorite/', views.toggle_favorite, name='toggle_favorite'),
 ]
